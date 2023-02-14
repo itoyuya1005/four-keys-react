801de87a211b47a2ed68aef3372f86ca8abd7864
itoyuya <ito.yuya@d-publishing.jp>
2023-02-14 18:27:05 +0900

    headder 途中

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -4,9 +4,6 @@ import Header from './components/Header'
 const App: React.FC = () => {
   return (
       <header className="header">
-        <div className="title-container">
-          <h1 className="title">Snake Game</h1>
-        </div>
         <Header />
       </header>
   )
